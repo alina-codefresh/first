d1f594831d03ec71457a4768ef9e9bb8ae380b8f
alina-codefresh <alina.shportiuk@codefresh.io>
2023-02-20 13:31:15 +0200

    test

---
+++ playground.js
@@ -0,0 +1,22 @@
+// let year = 2015;
+// let firstName = 'Den';
+
+// let cond1 = (year == 2015); // equality evaluates to true or false
+// let cond2 = (firstName == 'Alina');
+// let rightUser = (cond1 && cond2)
+
+// if (rightUser) {
+//     console.log('truthy')
+// } else {
+//     console.log('falsy')
+// }
+
+let year = 2015;
+
+if (year < 2015) {
+  alert( 'Too early...' );
+} else if (year > 2015) {
+  alert( 'Too late' );
+} else {
+  alert( 'Exactly!' );
+}
